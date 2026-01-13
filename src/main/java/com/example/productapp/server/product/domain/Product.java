83314ab9fb067175f620f2d2675414b135200a3c
roberto9985 <lefter.roberto99@yahoo.com>
2026-01-13 21:44:15 +0200

    add product entity

---
+++ src/main/java/com/example/productapp/server/product/domain/Product.java
@@ -0,0 +1,29 @@
+package com.example.productapp.server.product.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+}
