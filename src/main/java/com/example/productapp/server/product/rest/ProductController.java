f43e379fb370fa76d0cef15bcac5df09edf18e11
roberto9985 <lefter.roberto99@yahoo.com>
2026-01-15 09:49:27 +0200

    add logging with slf4j

--- src/main/java/com/example/productapp/server/product/rest/ProductController.java
+++ src/main/java/com/example/productapp/server/product/rest/ProductController.java
@@ -4,6 +4,7 @@ import com.example.productapp.server.product.rest.dto.ProductRequestResponse;
 import com.example.productapp.server.product.rest.mapper.ProductMapper;
 import com.example.productapp.server.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +14,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
+@Slf4j
 public class ProductController {
 
     private final ProductService productService;
 
     @PostMapping
     public ResponseEntity<ProductRequestResponse> createProduct(@RequestBody ProductRequestResponse request) {
+        log.info("REST request for creating product with SKU: {}", request.sku());
+
         ProductRequestResponse response =
                 ProductMapper.productToProductRequestResponse(productService.create(ProductMapper.productRequestResponseToProduct(request)));
         return new ResponseEntity<>(response, HttpStatus.CREATED);
@@ -26,11 +30,15 @@ public class ProductController {
 
     @GetMapping
     public ResponseEntity<List<ProductRequestResponse>> getAllProducts() {
+        log.info("REST request for getting all products");
+
         return ResponseEntity.ok(ProductMapper.productsToProductRequestResponses(productService.getAll()));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ProductRequestResponse> getProductById(@PathVariable Long id) {
+        log.info("REST request for fetching product with id: {}", id);
+
         return ResponseEntity.ok(ProductMapper.productToProductRequestResponse(productService.getById(id)));
     }
 
--- src/main/java/com/example/productapp/server/product/service/ProductService.java
+++ src/main/java/com/example/productapp/server/product/service/ProductService.java
@@ -5,6 +5,7 @@ import com.example.productapp.server.product.exception.ProductAlreadyExistsExcep
 import com.example.productapp.server.product.exception.ProductNotFoundException;
 import com.example.productapp.server.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
@@ -12,31 +13,53 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
 
     private final ProductRepository productRepository;
 
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public Product create(Product product) {
+        log.debug("Create product with SKU: {}", product.getSku());
+
         if (productRepository.existsBySku(product.getSku())) {
+            log.debug("Product with SKU {} already exists, throwing exception", product.getSku());
             throw new ProductAlreadyExistsException("Product with SKU " + product.getSku() + " already exists.");
         }
+
+        log.debug("Product created successfully: {}", product);
         return productRepository.save(product);
     }
 
     @PreAuthorize("hasAnyAuthority('ROLE_USER', 'ROLE_ADMIN')")
     public Product getById(Long id) {
-        return productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
+        log.debug("Fetching product by id: {}", id);
+
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.debug("Product with id {} not found, throwing exception", id);
+                    return new ProductNotFoundException(id);
+                });
+
+        log.debug("Product fetched successfully: {}", product);
+        return product;
     }
 
     @PreAuthorize("hasAnyAuthority('ROLE_USER', 'ROLE_ADMIN')")
     public List<Product> getAll() {
-        return productRepository.findAll();
+        log.debug("Fetching all products");
+
+        List<Product> products = productRepository.findAll();
+        log.debug("Total products fetched: {}", products.size());
+
+        return products;
     }
 
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public void delete(Long id) {
+        log.debug("Deleting product with id: {}", id);
         productRepository.deleteById(id);
+        log.debug("Product with id {} deleted successfully", id);
     }
 
 }
